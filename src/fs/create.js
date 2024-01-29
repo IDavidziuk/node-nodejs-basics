6078331d251539b764516cbc158a20f6fdf41155
Ina-Davidziuk <ina.davidziuk@gmail.com>
2024-01-29 22:59:42 +0100

    fix: fix create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,9 +1,20 @@
-import { writeFile } from 'fs'
+import fs from 'fs'
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const filePath = join(__dirname, 'files', 'fresh.txt')
 
 const create = async () => {
-  writeFile('fresh.txt', 'I am fresh and young', { flag: 'wx' }, (err) => {
-    if (err) throw new Error('FS operation failed')
-  })
+  await fs.writeFile(
+    filePath,
+    'I am fresh and young',
+    { flag: 'wx+' },
+    (err) => {
+      if (err) throw new Error('FS operation failed')
+    }
+  )
+  console.log('File created')
 }
 
 await create()
